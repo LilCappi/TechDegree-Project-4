5c4aee020eb0b1b2156eb7c9f40e94e140c70370
Joshua Powell <frogman2013.jp@gmail.com>
2023-09-29 15:23:56 -0400

    Added classes, params, and methods
    Created necessary classes, parameters, and methods. Working on logic

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,19 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+    addPhraseToDisplay() {
+        const newPhrase = this.phrase.split('');
+        console.log(newPhrase);
+    }
+    checkLetter() {
+
+    }
+    showMatchedLetter() {
+
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const startGameBtn = document.getElementById('btn__reset');
+
+startGameBtn.addEventListener('click', () => {
+    const newPhrase = new Phrase('Hello all');
+    newPhrase.addPhraseToDisplay();
+})
